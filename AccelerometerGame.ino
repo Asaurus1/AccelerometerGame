4204e179439adaa9653ebcb55f8d248aa292c665
A Martin <a-martin@nusolar.org>
2017-05-22 01:39:04 -0500

    Added food

--- AccelerometerGame.ino
+++ AccelerometerGame.ino
@@ -1,3 +1,4 @@
+#include <LinkedList.h>
 #include <Matrix.h>
 #include <TFT_ST7735.h>
 
@@ -16,6 +17,7 @@ bool showSlidingDot_toggle = true;
 #define DOT_XSTART 64
 #define DOT_YSTART 64
 #define DOT_SIZE 5
+#define DOT_COLOR RED
 #define DOT_XMIN 0
 #define DOT_XMAX 127
 #define DOT_YMIN 10
@@ -31,23 +33,45 @@ struct obj_dot {
 Matrix AcclEndPoint(3,1);
 Matrix VelEndPoint(3,1);
 
-double accelerationFactor = 2;
+double accelerationFactor = 1.5;
 double dampingFactor = 0.9;
 double dotMaxVel = 10;
 
 /////////// Food Object Declaration
+#define FOOD_SIZE 2
+#define FOOD_COLOR YELLOW
+
 struct obj_food {
+  // Constructor
+  obj_food()  {position = Matrix(3,1);}
+  obj_food(int x, int y) { position = Matrix(3,1); position(0) = x; position(1) = y; }
+
+  // Members  
   Matrix position;
   char   color = BLUE;
+  bool   isWithinRadius(int x, int y, int r) { return pow(x-position(0),2) + pow(y-position(1),2) <= pow(r,2); }
+  static obj_food& createFood(int x, int y);
+  static void destroyFood(int index);
 };
 
-obj_food foodInstances[50];
-int      foodCount = 0;
+LinkedList<obj_food*> foodInstances;
 
+obj_food& obj_food::createFood(int x, int y) { 
+  obj_food* newFood = new obj_food(x, y);
+  foodInstances.add(newFood);
+  return *newFood;
+}
+
+void obj_food::destroyFood(int index) {
+  obj_food* deletingFood = foodInstances.remove(index);
+  delete deletingFood;
+}
 
 /////////// Game Variables
+#define   TIMEPERROUND 1000
 int       score = 0;
-long      timeRemaining = 1000;
+long      timeRemaining;
+enum      gs_var {RUNNING, GAMEOVER} gameState = RUNNING;
 
 /////////// Min and max ADC values { X, Y, Z };
 const int PROGMEM MINV[3] = {370, 413, 426};
@@ -69,26 +93,75 @@ void setup() {
   tft.begin();
   tft.setTextScale(2);
 
+  resetGame();  
+}
+
+void resetGame() {
   // Initialize Dot Position
   dot.position(0) = DOT_XSTART;
   dot.position(1) = DOT_YSTART;
+
+  // Remove existing food
+  while (foodInstances.size() > 0) {
+    delete foodInstances.pop();
+  }
+
+  // Create initial food on screen
+  randomSeed(analogRead(A0));
+  for (int i = 0; i < 12; i++) { obj_food::createFood(random(128),random(116)+12); }  
+
+  // clear screen
+  tft.clearScreen();
+
+  score = 0;
+  timeRemaining = TIMEPERROUND;
+  gameState = RUNNING;
+}
+
+void waitForShake(int timeout) {
+  long startTime = millis();
+  while (millis() < startTime + timeout && x_accl <1.3 && y_accl < 1.3) {readAcclData(); delay(20);}
 }
 
 void loop() {
   readAcclData();
 
   // Print to screen
-  clearSlidingDot();
-  dotMatrixMath();
-  drawSlidingDot();
-  drawVectors();
-  drawScorebar();
+  if (gameState == RUNNING) {
+    clearSlidingDot();
+    dotMatrixMath();
+    dotFoodCollisionHandle();
+    drawSlidingDot();
+    drawFood();
+    drawVectors();
+    drawScorebar();
+ 
+    // Create new food
+    if (random(100)<5) {
+      obj_food::createFood(random(128),random(116)+12);
+    }
 
-  // Countdown
-  timeRemaining -= 1;
+    // Countdown
+    timeRemaining -= 1;
+    if (timeRemaining <= 0) { gameState = GAMEOVER; }
+  } 
+  else {
+    tft.clearScreen();
+    tft.setTextScale(2);
+    tft.setTextColor(RED);
+    tft.setCursor(CENTER, 50, SCREEN);
+    tft.print("GAME OVER\n");
+    tft.setTextScale(1);
+    tft.setTextColor(WHITE);
+    tft.setCursor(CENTER, 80, SCREEN);
+    tft.print("SHAKE TO RETRY");
+    drawScorebar();
+    waitForShake(20000);
+    resetGame();
+  }
 
   //Wait till next gamestep
-  delay(30);
+  delay(20);
 }
 
 void drawScorebar() {
@@ -105,12 +178,20 @@ void drawScorebar() {
 }
 
 void drawSlidingDot() {
-  tft.fillCircle(dot.position(0),dot.position(1),DOT_SIZE,RED);
+  tft.fillCircle(dot.position(0),dot.position(1),DOT_SIZE,DOT_COLOR);
 }
 
 void drawVectors() {
   tft.drawLine(dot.position(0),dot.position(1),AcclEndPoint(0),AcclEndPoint(1),GREEN);
   tft.drawLine(dot.position(0),dot.position(1),VelEndPoint(0),VelEndPoint(1),BLUE);
+}
+
+void drawFood() {
+  int fSize = foodInstances.size();
+  for (int i = 0; i < fSize; i++) {
+    obj_food* f = foodInstances.get(i);
+    tft.fillCircle(f->position(0), f->position(1), FOOD_SIZE, FOOD_COLOR);
+  }
 }
 
 void clearSlidingDot() {
@@ -145,6 +226,17 @@ void dotWallCollisionHandle() {
   if (dot.position(1) == DOT_YMIN+DOT_SIZE && dot.velocity(1) < 0) {dot.velocity(1) = 0;}
   if (dot.position(0) == DOT_XMAX-DOT_SIZE && dot.velocity(0) > 0) {dot.velocity(0) = 0;}
   if (dot.position(1) == DOT_YMAX-DOT_SIZE && dot.velocity(1)> 0) {dot.velocity(1) = 0;}
+}
+
+void dotFoodCollisionHandle() {
+  for (int i = 0; i < foodInstances.size(); i++) {
+    obj_food* f = foodInstances.get(i);
+    if (f->isWithinRadius(dot.position(0),dot.position(1),DOT_SIZE+FOOD_SIZE)) {
+      tft.fillCircle(f->position(0), f->position(1), FOOD_SIZE, BLACK);
+      obj_food::destroyFood(i);
+      score++;
+    }
+  }
 }
 
 void readAcclData() {
